79f8594aa11e890f18bd95bf72fcd00fc0fa796c
Gabriela Hristova <gabriela.hr1406@gmail.com>
2022-12-19 17:42:44 +0200

    add

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,9 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
-
+    const products = Array.from(document.querySelectorAll(".product"));
+    const product = document.querySelector(".price");
+    products.forEach(x => {
+      x.setAttribute('data-price', price.textContent);
+    })
 });
